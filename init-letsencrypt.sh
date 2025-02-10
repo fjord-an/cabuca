fa223a8e4523dc6f58f3cf1c283c501c49a78208
Jordan <jordan.pacey@yahoo.com.au>
2025-02-10 21:14:24 +1000

    added admin image to compose file to orchestrate with the other services. confiugurations are baked in and build from the source code directory

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -19,36 +19,29 @@ else
 fi
 
 # Configuration via Environment Variables
-DOMAIN_NAMES="${DOMAIN_NAME}"
+DOMAINS=("-d ${DOMAIN_NAME}" "-d admin.${DOMAIN_NAME}")
 EMAIL="${EMAIL:-your-email@example.com}"
 RSA_KEY_SIZE="${RSA_KEY_SIZE:-4096}"
 STAGING="${STAGING:-0}"
 DATA_PATH="${DATA_PATH:-./certbot}"
 
-# Convert DOMAIN_NAMES into an array by splitting on commas.
-IFS=',' read -r -a domains <<< "$DOMAIN_NAMES"
-
-echo "Using the following domains: ${domains[@]}"
-echo "Using email: $EMAIL"
-echo "RSA key size: $RSA_KEY_SIZE"
-echo "Staging mode: $STAGING"
-
 echo "### Creating directories for SSL certificates and keys ..."
 mkdir -p "$DATA_PATH/www"
 mkdir -p "$DATA_PATH/conf"
 
-echo "### Starting Nginx ..."
-docker compose up --force-recreate -d nginx
+echo "### Starting Nginx and admin-frontend ..."
+docker compose -f docker-compose.prod.yml up --force-recreate -d nginx admin-frontend
 
-echo "### Requesting Let's Encrypt certificate for domains: ${domains[@]} ..."
-docker compose run --rm certbot certonly --webroot \
+echo "### Requesting Let's Encrypt certificates for domains: ${DOMAIN_NAME}, admin.${DOMAIN_NAME} ..."
+docker compose -f docker-compose.prod.yml run --rm certbot certonly --webroot \
   -w /var/www/certbot \
   $([[ $STAGING != "0" ]] && echo "--staging") \
   $([[ -n $EMAIL ]] && echo "--email $EMAIL" || echo "--register-unsafely-without-email") \
-  $(printf -- ' -d %s' "${domains[@]}") \
-  --rsa-key-size $RSA_KEY_SIZE \
+  "${DOMAINS[@]}" \
+  --rsa-key-size "$RSA_KEY_SIZE" \
   --agree-tos \
   --force-renewal
 
-echo "### Reloading Nginx ..."
-docker compose exec nginx nginx -s reload
+echo "### Reloading Nginx services ..."
+docker compose -f docker-compose.prod.yml exec nginx nginx -s reload
+docker compose -f docker-compose.prod.yml exec admin-frontend nginx -s reload
